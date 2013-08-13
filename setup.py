d36488466c68d5d18511d6eb6cd549aec24b403b
Constantine Evans <cge@dna.caltech.edu>
2013-08-13 10:50:48 -0700

    Additions to setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,17 @@ stickyext = Extension('stickydesign._stickyext', sources=['src/_stickyext.c'],
 # run the setup
 setup(
     name='stickydesign',
-    version='0.2',
+    version='0.2dev',
+
+    install_requires = ['numpy'],
+
     packages=['stickydesign'],
     ext_modules=[stickyext],
     package_dir = {'stickydesign': 'src'},
-    package_data={'stickydesign': ['params/dnastackingbig.csv']}
+    package_data={'stickydesign': ['params/dnastackingbig.csv']},
+
+    author = "Constantine Glen Evans",
+    author_email = "cevans@dna.caltech.edu",
+    description = "StickyDesign DNA Tile Sticky End Package",
+    url = 'http://dna.caltech.edu/stickydesign'
 )
