19b1e3f10d5eb43c2199375714df9c5676caadc3
Constantine Evans <cge@dna.caltech.edu>
2015-02-23 14:52:35 -0800

    fix installation issues, bump version

--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@ from setuptools import setup, Extension
 import numpy
 
 # define the extension module
-stickyext = Extension('stickydesign._stickyext', sources=['src/_stickyext.c'],
+stickyext = Extension('stickydesign._stickyext', sources=['stickydesign/_stickyext.c'],
                           include_dirs=[numpy.get_include()])
 
 # run the setup
 setup(
     name='stickydesign',
-    version='0.3dev',
+    version='0.3.1',
 
     install_requires = ['numpy'],
 
     packages=['stickydesign'],
     ext_modules=[stickyext],
-    package_dir = {'stickydesign': 'src'},
     package_data={'stickydesign': ['params/dnastackingbig.csv']},
 
     author = "Constantine Glen Evans",
     author_email = "cevans@dna.caltech.edu",
     description = "StickyDesign DNA Tile Sticky End Package",
-    url = 'http://dna.caltech.edu/StickyDesign'
+    url = 'http://dna.caltech.edu/StickyDesign',
+    zip_safe = True
 )
