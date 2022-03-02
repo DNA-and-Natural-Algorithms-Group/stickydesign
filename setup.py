b6e36b4d65234f96a06113ffedbe79647a975f22
Constantine Evans <const@costi.eu>
2022-03-02 14:47:29 +0000

    add numpy include properly

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, Extension
-import numpy
-# from setuptools.command.build_ext import _build_ext
+from setuptools.command.build_ext import build_ext as _build_ext
 
 
-# class build_ext(_build_ext):
-#     def finalize_options(self):
-#         _build_ext.finalize_options(self)
-#         # Prevent numpy from thinking it is still in its setup process:
-#         import builtins # NOQA
-#         builtins.__NUMPY_SETUP__ = False
-#         import numpy # NOQA
-#         self.include_dirs.append(numpy.get_include())
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        import builtins # NOQA
+        builtins.__NUMPY_SETUP__ = False
+        import numpy # NOQA
+        self.include_dirs.append(numpy.get_include())
 
 
 # define the extension module
 stickyext = Extension(
     'stickydesign._stickyext',
-    sources=['src/stickydesign/_stickyext.c'],
-    include_dirs=[numpy.get_include()])
+    sources=['src/stickydesign/_stickyext.c'])
+#    include_dirs=[numpy.get_include()])
 
 # run the setup
 setup(
@@ -32,6 +31,7 @@ setup(
         },
     packages=['stickydesign', 'stickydesign.stickydesign2'],
     ext_modules=[stickyext],
+    cmdclass={'build_ext':build_ext},
     package_data={'stickydesign': ['params/dnastackingbig.csv']},
     author="Constantine Glen Evans",
     author_email="const@costi.eu",
